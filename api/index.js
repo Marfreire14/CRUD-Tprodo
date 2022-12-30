a6c154d176de65dec24ff5bc60cff2596c022e33
Marfreire14 <marfreire14@gmail.com>
2022-12-30 10:28:25 -0300

    quinto commint

--- api/index.js
+++ api/index.js
@@ -9,10 +9,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Length, X-Requested-With');
     app.use(cors());
+
     next();
 });
 
